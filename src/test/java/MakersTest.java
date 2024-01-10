2bbd86c2344ed41608f9ebaa22b25b2efb3490cf
Neil Studd <neil.studd@makers.tech>
2024-01-10 14:40:36 +0000

    Make the test pass

--- src/test/java/MakersTest.java
+++ src/test/java/MakersTest.java
@@ -23,7 +23,7 @@ public class MakersTest {
     @Test
     void shouldPrintPageTitle() {
         driver.get("https://makers.tech");
-        Assertions.assertEquals(driver.getTitle(),"Welcome to Makers");
+        Assertions.assertEquals("We Are Makers. Building The Future.", driver.getTitle());
     }
 
     @AfterAll
