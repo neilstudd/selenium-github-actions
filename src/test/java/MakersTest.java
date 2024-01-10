1f050e9484d2dc1683012ac626f2393213d1a25e
Neil Studd <neil.studd@makers.tech>
2024-01-10 14:04:48 +0000

    Add sample test

---
+++ src/test/java/MakersTest.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MakersTest {
+    private static FirefoxDriver driver;
+
+    @BeforeAll
+    static void launchBrowser() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+
+    // Your tests will go here!
+    @Test
+    void shouldPrintPageTitle() {
+        driver.get("https://makers.tech");
+        Assertions.assertEquals(driver.getTitle(),"We Are Makers. Building The Future.");
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        driver.quit();
+    }
+}
